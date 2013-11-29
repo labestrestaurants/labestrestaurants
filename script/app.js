a1f31b4bf01021dc138bf827d256f9b6318aea69
dariel <darielnoel@gmail.com>
2013-11-29 14:35:33 -0430

    FR3_Order_Restaurants_Categories

--- script/app.js
+++ script/app.js
@@ -1,35 +1,36 @@
 YUI().use(
 	'base', 'json','labr-view-container','autocomplete', 'autocomplete-highlighters',
+	'dd-constrain', 'dd-proxy', 'dd-drop',
 function(Y){
 	Y.namespace('LAbr');
 
 	var App = { CategoriesViews: {}},
 		categories = [
 			{
-				id: 'American',
+				name: 'American',
 				tittle: 'Americans Restaurants',
 				dataRetrieve: {
 						location: 'los angeles, ca',
 						query: 'burger',
-						count:5
+						count:2
 				}				
 			},
 			{
-				id: 'Japanese',
+				name: 'Japanese',
 				tittle: 'Japanese Restaurants',
 				dataRetrieve: {
 						location: 'los angeles, ca',
 						query: 'japanese',
-						count:5
+						count:2
 				}				
 			},
 			{
-				id: 'Korean',
+				name: 'Korean',
 				tittle: 'Korean Restaurants',
 				dataRetrieve: {
 						location: 'los angeles, ca',
 						query: 'korean',
-						count:5
+						count:2
 				}				
 			}
 		],
@@ -48,8 +49,10 @@ function(Y){
 	App.init = function(config){
 		var views = App.CategoriesViews;
 		Y.each(categories, function(item){
-			views[item.id] = new Y.LAbr.Container(item).render('.panels-container');
-		});		
+			views[item.name] = new Y.LAbr.Container(item).render('.panels-container');
+		});	
+
+		App.reorderPanels();	
 	};
 
 	/**
@@ -63,8 +66,29 @@ function(Y){
 		Y.each(categories, function(item){
 			item.filterItems(query);
 		});
+	};
+
+	App.reorderPanels = function(){
+		//Get the list of li's in the lists and make them draggable
+		var lis = Y.Node.all('.panel');
+		lis.each(function(v, k) {
+		    var dd = new Y.DD.Drag({
+		        node: v,
+		        //Make it Drop target and pass this config to the Drop constructor
+		        target: {
+		            padding: '0 0 0 20'
+		        }
+		    }).plug(Y.Plugin.DDProxy, {
+		        //Don't move the node at the end of the drag
+		        moveOnEnd: false
+		    }).plug(Y.Plugin.DDConstrained, {
+		        //Keep it inside the #play node
+		        constrain2node: '.panels-container'
+		    }).addHandle('.drag-icon');
+		});		
 	}
 
+	//Filter Listeners
 	ac.on('query', function(e){
 		App.filterItems(e.query);
 	});
@@ -72,6 +96,61 @@ function(Y){
 	ac.on('clear', function(e){
 		App.filterItems('');
 	});
+
+	//Thanks to YUI Guys http://yuilibrary.com/yui/docs/dd/list-drag.html example
+	Y.DD.DDM.on('drag:start', function(e) {
+	    //Get our drag object
+	    var drag = e.target;
+	    //Set some styles here
+	    drag.get('node').setStyle('opacity', '.25');
+	    drag.get('dragNode').set('innerHTML', drag.get('node').get('innerHTML'));
+	    drag.get('dragNode').setStyles({
+	        opacity: '.5',
+	        borderColor: drag.get('node').getStyle('borderColor'),
+	        backgroundColor: drag.get('node').getStyle('backgroundColor')
+	    });
+	});
+
+	Y.DD.DDM.on('drag:end', function(e) {
+	    var drag = e.target;
+	    //Put our styles back
+	    drag.get('node').setStyles({
+	        visibility: '',
+	        opacity: '1'
+	    });
+	});
+
+	Y.DD.DDM.on('drop:over', function(e) {
+	    //Get a reference to our drag and drop nodes
+	    var drag = e.drag.get('node'),
+	        drop = e.drop.get('node');
+	    
+	    //Are we dropping on a panel node?
+	    if (drop.hasClass('panel')) {
+	        //Are we not going up?
+	        if (!goingUp) {
+	            drop = drop.get('nextSibling');
+	        }
+	        //Add the node to this list
+	        e.drop.get('node').get('parentNode').insertBefore(drag, drop);
+	        //Resize this nodes shim, so we can drop on it later.
+	        e.drop.sizeShim();
+	    }
+	});
+
+	Y.DD.DDM.on('drag:drophit', function(e) {
+	    var drop = e.drop.get('node'),
+	        drag = e.drag.get('node');
+
+	    //if we are not on an panel, we must have been dropped on a ul
+	    if (!drop.hasClass('panel')) {
+	        if (!drop.contains(drag)) {
+	            drop.appendChild(drag);
+	        }
+	    }
+	});
+
+	var goingUp = false, lastY = 0;
 
 	App.init();
 });
